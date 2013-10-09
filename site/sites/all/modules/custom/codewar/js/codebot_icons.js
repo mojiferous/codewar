5762cbea3d4527a951fa936442259859b2cdb323
mojfierous <mojiferous@mojiferous.com>
2013-10-09 16:52:30 -0600

    Improvements in the icon editor

--- site/sites/all/modules/custom/codewar/js/codebot_icons.js
+++ site/sites/all/modules/custom/codewar/js/codebot_icons.js
@@ -4,11 +4,14 @@
  * 8/23/12 Mojiferous
  */
 var hidden_icon;
+var has_hidden;
 var chooser_canvas;
 var icon_canvas;
 var tot_icons;
 
 (function ($, Drupal, window, document) {
+
+  has_hidden = false;
 
   Drupal.behaviors.codeBot = {
     attach: function (context) {
@@ -29,48 +32,119 @@ var tot_icons;
         chooser_canvas = document.getElementById('chooser-canvas').getContext('2d');
         icon_canvas = document.getElementById('icon-canvas').getContext('2d');
 
-        var icon_num = 0;
+        tot_icons = $('#icon-hidey-hole').children('img').length;
 
-        //add each icon to the icon chooser
-        $('#icon-hidey-hole').children('img').each(function() {
-          var this_img_id = $(this).attr('id');
-          var this_img = document.getElementById(this_img_id);
+        draw_chooser_icons();
+        draw_icon_canvas();
 
-          //draw the unadulterated image onto the hidden image canvas
-          hidden_icon.drawImage(this_img, 0, icon_num*32);
+        $('#chooser-canvas').click(function(event_obj) {
+          //the chooser canvas has been clicked
 
-          //draw the icon
-          draw_icon(this_img, icon_num, false);
+          var pos_select = Math.floor(event_obj.offsetY/26);
+          if(pos_select < tot_icons) {
+            //if we are clicking on an existing icon, redraw it here
+            Drupal.settings.codewar.selected_icon = pos_select;
+            draw_chooser_icons();
+            draw_icon_canvas();
+          } else if(pos_select == tot_icons) {
+            //the user has clicked on the add icon
 
-          icon_num++;
+            if(tot_icons < 10) {
+              Drupal.settings.codewar.selected_icon = tot_icons;
+              tot_icons++;
+              draw_chooser_icons();
+              draw_icon_canvas();
+            }
+
+          }
+
         });
 
-        //draws a + icon for new icons
-        draw_icon(null, icon_num, true);
+        $('#icon-canvas').mousedown(function(event_obj) {
+          //the icon canvas has been clicked, color the square
+          var cur_color = $('#chose-color').spectrum("get").toHexString();
 
-        tot_icons = icon_num-1;
+          if(cur_color != null && cur_color != '') {
+            //make sure we actually have a color
+            var x_val = Math.floor(event_obj.offsetX/11);
+            var y_val = Math.floor(event_obj.offsetY/11);
 
-        icon_canvas.fillStyle = '#999999';
-        icon_canvas.fillRect(0, 0, 354, 354);
+            icon_canvas.fillStyle = cur_color;
+            icon_canvas.fillRect((x_val*10)+(x_val+1), (y_val*10)+(y_val+1), 10, 10);
+          }
 
-        icon_canvas.strokeStyle = '#AAAAAA';
-        icon_canvas.lineWidth = .5;
-        icon_canvas.beginPath();
-        for(var n=0; n<33; n++) {
-          icon_canvas.moveTo(0, n*10+1);
-          icon_canvas.lineTo(354, n*10+1);
-          icon_canvas.moveTo(n*10+1, 0);
-          icon_canvas.lineTo(n*10+1, 354);
-        }
-        icon_canvas.stroke();
 
-        draw_image_data_on_canvas();
+
+        });
 
       });
 
     }
   };
 
+  /**
+   * draws the icon associated with Drupal.settings.codewar.selected_icon onto the icon_canvas
+   */
+  function draw_icon_canvas() {
+    icon_canvas.fillStyle = '#999999';
+    icon_canvas.fillRect(0, 0, 354, 354);
+
+    icon_canvas.strokeStyle = '#AAAAAA';
+    icon_canvas.lineWidth = .5;
+    icon_canvas.beginPath();
+    for(var n=0; n<33; n++) {
+      icon_canvas.moveTo(0, (n*10)+(n+1));
+      icon_canvas.lineTo(354, (n*10)+(n+1));
+      icon_canvas.moveTo((n*10)+(n+1), 0);
+      icon_canvas.lineTo((n*10)+(n+1), 354);
+    }
+    icon_canvas.stroke();
+
+    if(has_hidden) {
+      draw_image_data_on_canvas();
+    }
+
+  }
+
+  /**
+   * renders all the current chooser icons
+   */
+  function draw_chooser_icons() {
+    var icon_num = 0;
+
+    var hidey_hole = $('#icon-hidey-hole').children('img');
+
+    //add each icon to the icon chooser
+    for(var n=0; n<tot_icons; n++) {
+      var this_img = null;
+
+      if(hidey_hole[n] != null) {
+        //there is an icon in the hidey hole for this icon_num
+        var this_img_id = $(hidey_hole[n]).attr('id');
+        this_img = document.getElementById(this_img_id);
+        //draw the unadulterated image onto the hidden image canvas
+        hidden_icon.drawImage(this_img, 0, icon_num*32);
+        has_hidden = true;
+      } else {
+        has_hidden = false;
+      }
+
+      //draw the icon
+      draw_icon(this_img, icon_num, false);
+
+      icon_num++;
+    }
+
+    if(tot_icons < 10) {
+      //draws a + icon for new icons, but only if we have less than 10 icons
+      draw_icon(null, icon_num, true);
+    }
+
+  }
+
+  /**
+   * draws the image data from the selected icon onto the canvas
+   */
   function draw_image_data_on_canvas() {
     for(var x=0; x<32; x++) {
       for(var y=0; y<32; y++) {
@@ -116,7 +190,10 @@ var tot_icons;
 
     if(!is_new) {
       //this is an existing icon, render the passed image
-      chooser_canvas.drawImage(this_img, 0, 0, 32, 32, 4, (26*icon_num)+4, 16, 16);
+      if(this_img != null) {
+        chooser_canvas.drawImage(this_img, 0, 0, 32, 32, 4, (26*icon_num)+4, 16, 16);
+      }
+
     } else {
       //this is a new icon, make a + icon and "add new" text
       chooser_canvas.strokeStyle = '#FFFFFF';
